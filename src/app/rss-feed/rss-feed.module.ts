6b2d2afd1ab59225c0e1c48222853eeb78d14717
Abdurrahman Omar Elkousy <ab.omar@RMGS.CO>
2019-08-09 18:12:58 +0200

    added animation

--- src/app/rss-feed/rss-feed.module.ts
+++ src/app/rss-feed/rss-feed.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { CommonModule } from '@angular/common';
 import { RssFeedComponent } from './rss-feed/rss-feed.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RssFeedItemComponent } from './rss-feed-item/rss-feed-item.component';
 
-
-
 @NgModule({
   declarations: [RssFeedComponent, RssFeedItemComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   exports: [RssFeedComponent]
 })
--- src/app/rss-feed/rss-feed/rss-feed.component.ts
+++ src/app/rss-feed/rss-feed/rss-feed.component.ts
@@ -1,12 +1,28 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Item } from '../models/feed-api-response';
 import { ItemState, RssFeedService } from '../rss-feed.service';
 
 @Component({
   selector: 'app-rss-feed',
   templateUrl: './rss-feed.component.html',
   styleUrls: ['./rss-feed.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('showItem', [
+      state('in', style({
+        'opacity': 1
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateY(80px)'
+        })
+        , animate('200ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RssFeedComponent {
   vm$: Observable<ItemState> = this.rssFeedService.vm$;
@@ -16,5 +32,10 @@ export class RssFeedComponent {
 
   showMore() {
     this.rssFeedService.showSevenMoreItems();
+  }
+
+  // Method used to prevent the ngFor from rerendering items already existing in the list
+  trackByItemTitle(index: number, item: Item) {
+    return item.title;
   }
 }
